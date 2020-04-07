12f6d99490ed6115cce36657464dfa2fa7612031
j-greenberg <jgreenberg@runbox.com>
2020-04-07 19:56:40 -0400

    updated

--- data/router.js
+++ data/router.js
@@ -2,36 +2,31 @@ const express = require("express"); // import express
 const router = express.Router(); // new instance of Router
 const Data = require("./db.js");
 
-// | POST | /api/posts | Creates a post using the information sent inside the `request body`.
-// When the client makes a `POST` request to `/api/posts`:
-
-// - If the request body is missing the `title` or `contents` property:
-//   - cancel the request.
-//   - respond with HTTP status code `400` (Bad Request).
-//   - return the following JSON response: `{ errorMessage: "Please provide title and contents for the post." }`.
-
-// - If the information about the _post_ is valid:
-//   - save the new _post_ the the database.
-//   - return HTTP status code `201` (Created).
-//   - return the newly created _post_.
-
-// - If there's an error while saving the _post_:
-//   - cancel the request.
-//   - respond with HTTP status code `500` (Server Error).
-//   - return the following JSON object: `{ error: "There was an error while saving the post to the database" }`.
-
 router.post("/", (req, res) => {
-    console.log("POST request on /");
+    console.log("New POST request on /!");
 
     if(req.body.title && req.body.contents){
         console.log("Title and contents are included!"); 
-        console.log(Data.insert(req.body)); 
-
-    } else {
-
-        res.status(400).json({ errorMessage: "Please provide title and contents for the post." }); 
-
-    }
+            Data.insert(req.body)
+            .then(hub => {
+                res.status(201).json(hub); 
+                Data.find()
+                .then(response=>{
+                    console.log(response); 
+                }); 
+            })
+            .catch(error=>{
+                console.log(error); 
+                res.status(500).json({ error: "There was an error while saving the post to the database", });
+            })      
+        } else {
+            console.log("Title and contents were not provided! :("); 
+            Data.find()
+                .then(response=>{
+                    console.log(response); 
+                }); 
+            res.status(400).json({ errorMessage: "Please provide title and contents for the post." }); 
+            }
 })
 
 // | POST | /api/posts/:id/comments | Creates a comment for the post with the specified id using information sent inside of the `request body`.  
